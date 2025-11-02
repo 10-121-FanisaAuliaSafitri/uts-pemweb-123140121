54f07a8e1e71023f053610dd3ac658667dd75123
Fanisa Aulia Safitri <fanisa.123140121@student.itera.ac.id>
2025-11-02 16:27:09 +0700

    Add files via upload
    membuat api.jsx

---
+++ my-app/src/api.jsx
@@ -0,0 +1,3 @@
+export const API_KEY = "510f2f97610e04d028cd6486c5c7e122";
+export const BASE_URL = "https://api.openweathermap.org/data/2.5";
+export const GEO_URL = "https://api.openweathermap.org/geo/1.0";
